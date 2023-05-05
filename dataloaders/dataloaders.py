fdc100266133b9ea31412b4d6da2dd9904544eed
kiaraesguerra <kiaraesguerra@gmail.com>
2023-05-05 10:45:04 +0800

    Change avgpool to maxpool, remove paperaugment

--- dataloaders/dataloaders.py
+++ dataloaders/dataloaders.py
@@ -82,22 +82,7 @@ def get_transform(args):
         transforms.Resize(size=(36, 36))
     ]
     
-    
-    # if args.dataset != "svhn":
-    #     train_transform_list.append(
-    #         transforms.RandomCrop(size=(args.size, args.size), padding=args.padding)
-    #     )
-    
 
-    if args.paperaugment:
-        train_transform_list.extend([
-            transforms.RandomAffine(degrees=(0), shear=(0.2), translate=(0.2, 0.2), scale=(0.2, 0.2))
-        ])
-        
-    
-    
-    
-    
     if args.voltaaugment:
         train_transform_list.extend(
             [transforms.RandomHorizontalFlip()])
